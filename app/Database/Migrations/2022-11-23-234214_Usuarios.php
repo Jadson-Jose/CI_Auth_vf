64c7659eed86b82696711ba412763baa3a4af4a9
Jadson José da Silva <69395306+Jadson-Jose@users.noreply.github.com>
2022-11-23 21:02:38 -0300

    Update 2022-11-23-234214_Usuarios.php

--- app/Database/Migrations/2022-11-23-234214_Usuarios.php
+++ app/Database/Migrations/2022-11-23-234214_Usuarios.php
@@ -9,16 +9,50 @@ class Usuarios extends Migration
     public function up()
     {
         // Create users table
-        $this->forge->addFiend([
+        $this->forge->addField([
             'id' => [
                 'type' => 'int',
                 'constraint' => 11,
                 'unsigned' => true,
                 'auto_increment' => true
             ],
+            'username' => [
+                'type' => 'VARBINARY',
+                'constraint' => '200',
+                'null' => true
+            ],
+            'passwrd' => [
+                'type' => 'VARCHAR',
+                'constraint' => '1000',
+                'null' => true
+            ],
+            'profile' => [
+                'type' => 'VARCHAR',
+                'constraint' => '1000',
+                'null' => true
+            ],
+            'blocked_until' => [
+                'type' => 'DATETIME',
+                'null' => true
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => 'true'
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true
+            ],
+            'deleted_at' => [
+                'type' => 'DATETIME',
+                'null' => true
+            ]
 
-            
         ]);
+
+        // Create primary key
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('users');
     }
 
     public function down()
